d22253911e196540da51913e2ed3492c9d0e10a2
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-09-20 16:55:20 -0400

    pin_model and q0 are made optional in FlexivSim class

--- FlexivPy/robot/sim/sim_robot.py
+++ FlexivPy/robot/sim/sim_robot.py
@@ -9,7 +9,7 @@ import yaml
 import pinocchio as pin
 import FlexivPy.robot.dds.flexiv_messages as flexiv_messages
 
-ASSETS_PATH = "FlexivPy/assets/"
+from FlexivPy import ASSETS_PATH
 
 
 import cv2
@@ -37,11 +37,22 @@ class FlexivSim:
         object_names=[],
         joints=None,
         has_gripper=False,
-        camera_name = "static_camera"
+        camera_name = "static_camera", 
+        gravity = np.array([0., 0., -9.81])
     ):
-
+        self.dt = dt
         self.camera_name = camera_name
         self.has_gripper = has_gripper
+        self.gravity = gravity
+        self.render = render
+        self.step_counter = 0
+        if q0 is not None:
+            self.q0 = q0
+        else:
+            self.q0 = np.array([0.0, -0.698, 0.000, 1.571, -0.000, 0.698, -0.000])
+        # initialize the command with a zero buffer
+        self.cmd = flexiv_messages.FlexivCmd()
+        # Get all the joints if the joint list is not provided by the user
         if joints is None:
             self.joints = [
                 "joint1",
@@ -55,75 +66,70 @@ class FlexivSim:
         else:
             self.joints = joints
 
-        assert pin_model is not None
+        self.kv_damping = kv_damping
+        self.object_names = object_names
         self.CV2 = None
+        self.gravity_comp = gravity_comp
 
+        # Load a default scene if the scene XML is not provided by the
         if xml_path is None:
             self.model = mujoco.MjModel.from_xml_path(
                 os.path.join(ASSETS_PATH, "mjmodel.xml")
             )
         else:
             self.model = mujoco.MjModel.from_xml_path(xml_path)
-        self.simulated = True
-        self.data = mujoco.MjData(self.model)
-        self.dt = dt
-        _render_dt = 1.0 / 30.0
-        self.render_ds_ratio = max(1, _render_dt // dt)
-        self.camera_render_dt = 1.0 / 10.0
-        self.camera_render_ds_ratio = max(1, self.camera_render_dt // dt)
-        print("camera_render_ds_ratio", self.camera_render_ds_ratio)
-
-        self.kv_damping = kv_damping
-        self.object_names = object_names
-
-        if render:
-            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
-            self.render = True
-        else:
-            self.render = False
-
-        self.model.opt.gravity[2] = -9.81
-        self.model.opt.timestep = dt
-        self.renderer = None
-        self.render = render
-        self.step_counter = 0
-        self.cmd = None
-        self.pin_model = pin_model
-
-        self.gravity_comp = gravity_comp
-
-        # lets check that the model is correct!
-        pin.computeGeneralizedGravity(
-            self.pin_model.model, self.pin_model.data, np.zeros(7)
-        )
-
-        # self.reset()
-        mujoco.mj_forward(self.model, self.data)
-        if self.render:
-            self.viewer.sync()
-
-        self.q0 = q0
-        if self.q0 is not None:
-            self.reset_state_robot(q0, np.zeros(7))
-            if self.render:
-                self.viewer.sync()
-
+        
+        self.data = mujoco.MjData(self.model)    
+        self.model.opt.gravity = self.gravity.squeeze()
+        self.model.opt.timestep = self.dt    
+        # Enable camera simulation?
         self.last_camera_image = None
         if render_images:
             import cv2
-
             self.CV2 = cv2
             # TODO: adpat this code to include more camera if desired!
-            
-
             self.camera_renderer = mujoco.Renderer(self.model, 480, 640)
             self.camera_renderer.update_scene(self.data, camera=self.camera_name)
             pixels = self.camera_renderer.render()
             self.last_camera_image = cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR).copy()
+            self.camera_render_dt = 1.0 / 10.0
+            self.camera_render_ds_ratio = max(1, self.camera_render_dt // dt)
+            print("camera_render_ds_ratio", self.camera_render_ds_ratio)
         else:
             self.camera_renderer = None
+        # Enable live rendering?
+        if self.render:
+            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+            _render_dt = 1.0 / 30.0
+            self.render_ds_ratio = max(1, _render_dt // dt)
+        
+        # Load a default piochhio model if it's not provided by the user
+        if pin_model is not None:
+            self.pin_model = pin_model
+            try:
+                # lets check that the model is correct!
+                pin.computeGeneralizedGravity(
+                    self.pin_model.model, self.pin_model.data, self.q0
+                )
+            except:
+                assert False, 'The provided Pinocchio model is not right.'
+        else:
+            urdf = os.path.join(ASSETS_PATH, "flexiv_rizon10s_kinematics.urdf")
+            meshes_dir = os.path.join(ASSETS_PATH, "meshes")
+            from pinocchio.robot_wrapper import RobotWrapper
+            self.pin_model = RobotWrapper.BuildFromURDF(urdf, meshes_dir)
 
+        self.reset_state_robot(self.q0, np.zeros(7))
+        # Step once to update the visualizer
+        self.step()
+        if self.render:
+            self.viewer.sync()
         print("robot sim is ready!")
+
+    def get_gravity(self):
+        return pin.computeGeneralizedGravity(
+                self.pin_model.model, self.pin_model.data, self.get_robot_joints()
+            )
 
     def get_robot_joints(self):
         return np.array([self.data.joint(j).qpos[0] for j in self.joints])
@@ -169,6 +175,7 @@ class FlexivSim:
                 print(exc)
 
     def reset_state_robot(self, q, dq):
+        self.step_counter = 0
         self.set_robot_joints(q)
         self.set_robot_vel(dq)
         mujoco.mj_forward(self.model, self.data)
@@ -206,9 +213,7 @@ class FlexivSim:
         )
 
         if not cmd.tau_ff_with_gravity:
-            tau += pin.computeGeneralizedGravity(
-                self.pin_model.model, self.pin_model.data, self.get_robot_joints()
-            )
+            tau += self.get_gravity()
 
         if self.kv_damping > 0:
             tau -= self.kv_damping * self.get_robot_vel()
